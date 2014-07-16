8305266990d966d0035080630420f18f03719c95
Ventsislav Yordanov <venciity@abv.bg>
2014-07-16 18:10:47 +0300

    JavaScriptSyntax

---
+++ 03.JavaScript Basics/02.JavaScriptSyntax/13.DigitalSoothsayer/DigitalSoothsayer.js
@@ -0,0 +1,12 @@
+﻿function soothsayer(work, language, city, car) {
+    var randomWork = work[Math.floor(Math.random() * work.length)];
+    var randomLanguage = language[Math.floor(Math.random() * language.length)];
+    var randomCity = city[Math.floor(Math.random() * city.length)];
+    var randomCar = car[Math.floor(Math.random() * car.length)];
+
+    return "You will work " + randomWork + " years on " + randomLanguage +
+        ".\nYou will live in " + randomCity + " and drive " + randomCar + ".";
+}
+
+console.log(soothsayer([3, 5, 2, 7, 9], ["Java", "Python", "C#", "JavaScript", "Ruby"],
+    ["Silicon Valley", "London", "Las Vegas", "Paris", "Sofia"], ["BMW", "Audi", "Lada", "Skoda", "Opel"]));
---
+++ 03.JavaScript Basics/02.JavaScriptSyntax/14.CalculateExpression/calculate.js
@@ -0,0 +1,4 @@
+﻿function calculate() {
+    var result = eval(document.getElementById("input").value);
+    document.getElementById("result").innerHTML = result;
+}
