9a0b905dc708585c1759ed76da172dbd9f8ef2f2
Ventsislav Yordanov <venciity@abv.bg>
2014-08-24 03:23:33 +0300

    ArraysStringsObjects

--- 04.PHP BASICS/04.ArraysStringsObjects/05.SentenceExtractor.php
+++ 04.PHP BASICS/04.ArraysStringsObjects/05.SentenceExtractor.php
@@ -20,13 +20,13 @@
             $word = $_POST['word'];
 
             $sentence = "";
-            $arraySentence = [];
+            $WordsInSentence = [];
 
             for ($i = 0; $i < strlen($text); $i++) {
                 if ($text[$i] == "." || $text[$i] == "?" || $text[$i] == "!") {
                     $sentence .= $text[$i];
-                    $arraySentence = preg_split("/([^a-zA-Z])/", $sentence , -1 , PREG_SPLIT_NO_EMPTY);
-                    if (in_array($word ,$arraySentence)) {
+                    $WordsInSentence = preg_split("/([^a-zA-Z])/", $sentence , -1 , PREG_SPLIT_NO_EMPTY);
+                    if (in_array($word ,$WordsInSentence)) {
                         echo "$sentence </br>";
                     }
                     $sentence = "";
