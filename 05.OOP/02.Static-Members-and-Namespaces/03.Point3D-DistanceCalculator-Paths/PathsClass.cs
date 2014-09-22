a7eda2d7a8fee9f2cbcd4865942e214a6fe20fdf
Ventsislav Yordanov <venciity@abv.bg>
2014-09-22 15:49:03 +0300

    Static-Members-and-Namespaces

---
+++ 05.OOP/02.Static-Members-and-Namespaces/03.Point3D-DistanceCalculator-Paths/PathsClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space3D
+{
+    class PathsClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Point3D.StartingPoint.ToString());
+
+            Point3D rectA = new Point3D("A", 1.5, 3, 5.3);
+            Point3D rectB = new Point3D("B", 2.5, -3, 5.6);
+            Point3D rectC = new Point3D("C", 3.5, 0, 10);
+            Point3D rectD = new Point3D("D", -4.5, 8, 8.3);
+
+            Path3D path = new Path3D(rectA, rectB, rectC, rectD);
+            Console.WriteLine(path.ToString());
+
+            Console.WriteLine(DistanceCalculator.CalculateDistance3D(rectA, rectD));
+
+            // storage tests
+            Storage.SavePath(@"../../userFiles/SavedPaths.txt", false, path);
+            //Storage.SavePath(@"../userFiles/SavedPaths.txt", true, path);
+
+            //var loadedList = Storage.LoadPaths(@"../../userFiles/SavedPaths.txt");
+            //loadedList.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
---
+++ 05.OOP/02.Static-Members-and-Namespaces/03.Point3D-DistanceCalculator-Paths/Storage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space3D
+{
+    static class Storage
+    {
+        public static void SavePath(string fullFileName, bool append, Path3D path)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fullFileName, append, Encoding.GetEncoding("UTF-8")))
+                {
+                    streamWriter.WriteLine(path.ToString());
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw exception.InnerException;
+            }
+        }
+
+        public static List<Path3D> LoadPaths(string fullFileName)
+        {
+            try
+            {
+                List<Path3D> paths = new List<Path3D>();
+                using (StreamReader streamReader = new StreamReader(fullFileName, Encoding.GetEncoding("UTF-8")))
+                {
+                    string line = streamReader.ReadLine();
+                    while (line != null)
+                    {
+                        Path3D points = new Path3D();
+                        var lines = line.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (var lin in lines)
+                        {
+                            points.Add(Point3D.Deserialize(lin));
+                        }
+
+                        line = streamReader.ReadLine();
+                        paths.Add(points);
+                    }
+                }
+                return paths;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw exception.InnerException;
+            }
+        }
+    }
+}
