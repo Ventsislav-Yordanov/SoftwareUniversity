d4612a60adf1c2c63eb71634ce7c916c86612ead
Ventsislav Yordanov <venciity@abv.bg>
2014-09-24 15:44:38 +0300

    Exception-Handling

---
+++ 05.OOP/02.Exception-Handling/01.Square Root/SquareRoot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ class SquareRoot
+ {
+     static void Main(string[] args)
+     {
+         try
+         {
+            Console.Write("Enter a number : ");
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine("Square root from number : {0} is {1}", number, squareRoot);
+         }
+         catch (ArgumentNullException)
+         {
+             Console.Error.WriteLine("Invalid Number!");
+         }
+         catch (OverflowException)
+         {
+             Console.Error.WriteLine("Too long Number! [ overflow ]");
+         }
+         catch (FormatException)
+         {
+             Console.Error.WriteLine("Invalid Number!");
+         }
+         finally
+         {
+             Console.WriteLine("Good bye!");
+         }
+     }
+ }
---
+++ 05.OOP/02.Exception-Handling/02.EnterNumbers/EnterNumbersClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    public class EnterNumbersClass
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            int counter = 0;
+
+            Console.WriteLine("Please enter 10 numbers :");
+            while (counter < 10)
+            {
+                try
+                {
+                    int currentNumber = ReadNumbers(start, end);
+                    if (currentNumber > start)
+                    {
+                        start = currentNumber;
+                    }
+                    counter++;
+                }
+                catch (ArgumentException argumentException)
+                {
+                    Console.Error.WriteLine("{0} Repeat input!", argumentException.Message);
+
+                }
+                catch (Exception exception)
+                {
+                    Console.Error.WriteLine("{0} Repeat input!", exception.Message);
+                }
+            }
+        }
+
+        public static int ReadNumbers(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start || number > end)
+            {
+                throw new IndexOutOfRangeException("The number is outside of the requested range!");
+            }
+            return number;
+        }
+    }
+}
