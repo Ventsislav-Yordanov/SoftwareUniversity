1633ce9ed840e5079348708cca94b72d293dac3d
Ventsislav Yordanov <venciity@abv.bg>
2014-09-28 22:39:39 +0300

    Revert "Revert "Delegates-and-Events""
    This reverts commit d4d750d70206c1e8ff9baa890ac586442ab8aa80.

---
+++ 05.OOP/03.Delegates-and-Events/AsynchronousTimer/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace AsynchronousTimer
+{
+    public class Timer
+    {
+        public static void Work1(int num)
+        {
+            Console.WriteLine(num);
+        }
+
+        public static void Work2(int num)
+        {
+            Console.Beep();
+        }
+
+        static void Main(string[] args)
+        {
+            AsyncTimer timer1 = new AsyncTimer(Work1, 1000, 10);
+            timer1.Start();
+
+            AsyncTimer timer2 = new AsyncTimer(Work2, 900, 10);
+            timer2.Start();
+        }
+    }
+}
