c8e7187e7b90b03d006ba6fbcc62e103554261e4
Ventsislav Yordanov <venciity@abv.bg>
2014-11-29 22:55:55 +0200

    update code

--- 07.JavaScript Applications/WebStorages/GreetingSystem/application.js
+++ 07.JavaScript Applications/WebStorages/GreetingSystem/application.js
@@ -1,10 +1,33 @@
-﻿(function () {
-    $form = $('#inputForm');
-    if ($name = $('#name').val() != "") {
-        if (!sessionStorage.name) {
-            $name = $('#name').val();
-            sessionStorage.setItem('name', $name);
+﻿$(document).ready(function () {
+    if (localStorage.name) {
+        $('form').hide();
+        $('#info').text('Hello ' + localStorage.name +
+            ' session visits count:' + sessionStorage.sessionVisitscount +
+            ' total visits count :' + localStorage.totalVisitscount);
+    }
+
+    $('#submit').on('click', function () {
+        localStorage.name = $('#name').val();
+    })
+
+    function incrementSessionCount() {
+        if (sessionStorage.sessionVisitscount) {
+            sessionStorage.sessionVisitscount++;
+        } else {
+            sessionStorage.sessionVisitscount = 1
         }
     }
+
+    function incrementTotalCount() {
+        if (localStorage.totalVisitscount) {
+            localStorage.totalVisitscount++;
+        } else {
+            localStorage.totalVisitscount = 1
+        }
+    }
+
+    incrementTotalCount();
+    incrementSessionCount();
+})
     
-}());
+    
