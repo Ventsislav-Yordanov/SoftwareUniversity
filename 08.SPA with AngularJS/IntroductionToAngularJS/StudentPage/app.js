40b50060417f9d4933c005f2e18b5014c5f37d80
Ventsislav Yordanov <venciity@abv.bg>
2014-12-21 15:18:43 +0200

    update some code

--- 08.SPA with AngularJS/IntroductionToAngularJS/StudentPage/app.js
+++ 08.SPA with AngularJS/IntroductionToAngularJS/StudentPage/app.js
@@ -1,12 +1,12 @@
 (function(){
-    var student = { "name": "Pesho",
-        "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
-        "grade": 5,
-        "school": "High School of Mathematics",
-        "teacher": "Gichka Pesheva"
-    };
     var app = angular.module('appModule', []);
-    app.controller('StudentController', function(){
-        this.student = student;
+    app.controller('StudentController', function($scope){
+        $scope.student = {
+            "name": "Pesho",
+            "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+            "grade": 5,
+            "school": "High School of Mathematics",
+            "teacher": "Gichka Pesheva"
+        };
     });
 })();
