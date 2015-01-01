27513712b945ac5dd2cdf2d31397ae5cbeb20835
Ventsislav Yordanov <venciity@abv.bg>
2015-01-01 20:42:54 +0200

    delete unused code and commit Ads Skeleton

---
+++ 08.SPA with AngularJS/Ads [ AngularJS project ]/Server/Ads.Data/IAdsData.cs
@@ -0,0 +1,20 @@
+﻿namespace Ads.Data
+{
+    using Ads.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public interface IAdsData
+    {
+        IRepository<ApplicationUser> Users { get; }
+
+        IRepository<IdentityRole> UserRoles { get; }
+
+        IRepository<Advertisement> Ads { get; }
+
+        IRepository<Town> Towns { get; }
+
+        IRepository<Category> Categories { get; }
+
+        int SaveChanges();
+    }
+}
