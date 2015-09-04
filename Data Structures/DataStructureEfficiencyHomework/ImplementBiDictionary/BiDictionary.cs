3ea5a85d98bb3cb8f9b9581acf9f9247847c4d26
Venciity <venciity@abv.bg>
2015-09-04 19:09:45 +0300

    Add Data Structure Efficiency Homework

---
+++ Data Structures/DataStructureEfficiencyHomework/ImplementBiDictionary/BiDictionary.cs
@@ -0,0 +1,65 @@
+﻿namespace ImplementBiDictionary
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BiDictionary<TKey1, TKey2, TValue>
+    {
+        private Dictionary<TKey1, List<TValue>> valuesByFirstKey = new Dictionary<TKey1, List<TValue>>();
+        private Dictionary<TKey2, List<TValue>> valuesBySecondKey = new Dictionary<TKey2, List<TValue>>();
+        private Dictionary<Tuple<TKey1, TKey2>, List<TValue>> valuesByBothKeys = new Dictionary<Tuple<TKey1, TKey2>, List<TValue>>();
+
+        public void Add(TKey1 key1, TKey2 key2, TValue value)
+        {
+            this.valuesByFirstKey.AppendValueToKey(key1, value);
+
+            this.valuesBySecondKey.AppendValueToKey(key2, value);
+
+            var tuple = new Tuple<TKey1, TKey2>(key1, key2);
+            this.valuesByBothKeys.AppendValueToKey(tuple, value);
+        }
+
+        public IEnumerable<TValue> Find(TKey1 key1, TKey2 key2)
+        {
+            var tuple = new Tuple<TKey1, TKey2>(key1, key2);
+            return this.valuesByBothKeys.GetValuesForKey(tuple);
+        }
+
+        public IEnumerable<TValue> FindByKey1(TKey1 key1)
+        {
+            return this.valuesByFirstKey.GetValuesForKey(key1);
+
+        }
+
+        public IEnumerable<TValue> FindByKey2(TKey2 key2)
+        {
+            return this.valuesBySecondKey.GetValuesForKey(key2);
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2)
+        {
+            var tuple = new Tuple<TKey1, TKey2>(key1, key2);
+            if (!this.valuesByBothKeys.ContainsKey(tuple))
+            {
+                return false;
+            }
+
+            var values = this.valuesByBothKeys[tuple];
+            this.valuesByBothKeys.Remove(tuple);
+            foreach (var value in values)
+            {
+                if (this.valuesByFirstKey[key1].Contains(value))
+                {
+                    this.valuesByFirstKey[key1].Remove(value);
+                }
+
+                if (this.valuesBySecondKey[key2].Contains(value))
+                {
+                    this.valuesBySecondKey[key2].Remove(value);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Data Structures/DataStructureEfficiencyHomework/ImplementBiDictionary/DictionaryExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace ImplementBiDictionary
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Appends a new value to the collection of values mapped to the specified
+        /// dictionary key. If the collection does not exists for the specified key,
+        /// a new empty collection is first created and mapped to this key.
+        /// </summary>
+        /// <param name="key">The key that holds a collection of values</param>
+        /// <param name="value">The value to be added to the collection for this key</param>
+        public static void AppendValueToKey<TCollection, TKey, TValue>(
+            this IDictionary<TKey, TCollection> dict, TKey key, TValue value)
+            where TCollection : ICollection<TValue>, new()
+        {
+            TCollection collection;
+            if (!dict.TryGetValue(key, out collection))
+            {
+                collection = new TCollection();
+                dict.Add(key, collection);
+            }
+
+            collection.Add(value);
+        }
+
+        /// <summary>
+        /// Get a sequence of values assigned to the specified dictionary key.
+        /// If the key is missing, an empty sequence is returned.
+        /// </summary>
+        public static IEnumerable<TValue> GetValuesForKey<TKey, TValue>(
+            this IDictionary<TKey, List<TValue>> dict, TKey key)
+        {
+            List<TValue> collection;
+            if (dict.TryGetValue(key, out collection))
+            {
+                return collection;
+            }
+
+            return Enumerable.Empty<TValue>();
+        }
+    }
+}
