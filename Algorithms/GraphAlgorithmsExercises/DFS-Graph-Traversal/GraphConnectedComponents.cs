f5c34e08652fe91087c88557e3f96c95520acbf9
Venciity <venciity@abv.bg>
2015-10-24 16:55:48 +0300

    Added Graph algorithms exercises

---
+++ Algorithms/GraphAlgorithmsExercises/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph = new List<int>[]
+    {
+        new List<int>() { 3, 6 },
+        new List<int>() { 3, 4, 5, 6 },
+        new List<int>() { 8 },
+        new List<int>() { 0, 1, 5 },
+        new List<int>() { 1, 6 },
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 1, 4 },
+        new List<int>() { },
+        new List<int>() { 2 }
+    };
+
+    static bool[] visited;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+        //if (graph.Length == 0)
+        //{
+        //    Console.WriteLine("Connected component: ");
+        //}
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                Console.Write("Connected component:");
+                DepthFirstSearch(node);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void DepthFirstSearch(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            foreach (var child in graph[node])
+            {
+                DepthFirstSearch(child);
+            }
+            Console.Write(String.Format(" {0}", node));
+        }
+    }
+}
---
+++ Algorithms/GraphAlgorithmsExercises/Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private HashSet<string> visitedNodes;
+    private HashSet<string> cycleNodes;
+    private LinkedList<string> sortedNodes;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+
+    public ICollection<string> TopSort()
+    {
+        this.visitedNodes = new HashSet<string>();
+        this.sortedNodes = new LinkedList<string>();
+        this.cycleNodes = new HashSet<string>();
+        foreach (var node in this.graph.Keys)
+        {
+            TopSortDFS(node);
+        }
+
+        return this.sortedNodes;
+    }
+
+    private void TopSortDFS(string node)
+    {
+        if (this.cycleNodes.Contains(node))
+        {
+            throw new InvalidOperationException("A cycle detected in the graph");
+        }
+
+        if (!this.visitedNodes.Contains(node))
+        {
+            this.visitedNodes.Add(node);
+            this.cycleNodes.Add(node);
+
+            if (graph.ContainsKey(node))
+            {
+                foreach (var childNode in graph[node])
+                {
+                    TopSortDFS(childNode);
+                }
+            }
+
+            this.cycleNodes.Remove(node);
+            this.sortedNodes.AddFirst(node);
+        }
+    }
+
+    //public ICollection<string> TopSort()
+    //{
+    //    var predecessorsCount = new Dictionary<string, int>();
+    //    foreach (var node in this.graph)
+    //    {
+    //        if (!predecessorsCount.ContainsKey(node.Key))
+    //        {
+    //            predecessorsCount[node.Key] = 0;
+    //        }
+    //        foreach (var childNode in graph[node.Key])
+    //        {
+    //            if (!predecessorsCount.ContainsKey(childNode))
+    //            {
+    //                predecessorsCount[childNode] = 0;
+    //            }
+
+    //            predecessorsCount[childNode]++;
+    //        }
+    //    }
+
+    //    var removeNodes = new List<string>();
+    //    bool noteRemoved = true;
+    //    while (noteRemoved)
+    //    {
+    //        string nodeToRemove = graph.Keys.FirstOrDefault(k => predecessorsCount[k] == 0);
+    //        if (nodeToRemove == null)
+    //        {
+    //            // No more nodes for removal (with 0 predecessors)
+    //            break;
+    //        }
+
+    //        foreach (var childNode in graph[nodeToRemove])
+    //        {
+    //            predecessorsCount[childNode]--;
+    //        }
+    //        removeNodes.Add(nodeToRemove);
+    //        graph.Remove(nodeToRemove);
+    //    }
+
+    //    if (graph.Count > 0)
+    //    {
+    //        throw new InvalidOperationException("A cycle detected in the graph");
+    //    }
+
+    //    return removeNodes;
+    //}
+}
